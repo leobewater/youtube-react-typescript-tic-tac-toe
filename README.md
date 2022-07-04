a18939f70809dfc33c58db23dadcdd8f1e7b90a3
Leo <leoc2628@gmail.com>
2022-07-04 16:01:39 -0400

    fix css

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=MRe-5nVBPRo&ab_channel=IanLenehan
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,6 +1,12 @@
 function Game() {
   return (
-    <div>Tic Tac Toe Game Page</div>
+    <div className='h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500'>
+      <h1 className='text-center text-5xl mb-4 font-display text-white'>Tic Tac Toe Game Page</h1>
+      <div>
+        <div>Board Goes Here</div>
+        <div>Scores Go Here</div>
+      </div>
+    </div>
   )
 }
 
