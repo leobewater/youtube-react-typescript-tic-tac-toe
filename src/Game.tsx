2ea5826b3125637837664bed47ed94206aac6c71
Leo <leoc2628@gmail.com>
2022-07-04 16:40:21 -0400

    add onclick event to each cell

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import Square from './Square';
-const INITIAL_GAME_STATE = ["X", "O", "X", "", "", "", "", "", ""];
+const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""];
 
 function Game() {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE)
 
+  const handleCellClick = (event: any) => {
+    console.log("cell clicked!", event.target.getAttribute('data-cell-index'))
+  }
   return (
     <div className='h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500'>
       <h1 className='text-center text-5xl mb-4 font-display text-white'>Tic Tac Toe Game Page</h1>
       <div>
         <div className='grid grid-cols-3 gap-3 mx-auto w-96'>
           {gameState.map((player, index) => (
-            <Square key={index}>{player}</Square>
+            <Square key={index} onClick={handleCellClick} {...{ index, player }} />
           ))}
         </div>
         <div>Scores Go Here</div>
