7b55131c1e6aef8d6a41cf416b099dbc194d11b9
Leo <leoc2628@gmail.com>
2022-07-04 16:04:47 -0400

    extend custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Lobster', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
